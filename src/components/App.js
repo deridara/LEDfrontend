4f0138f415af03f7156d24b0ccfeb064733db2a0
dara <teriaderide@gmail.com>
2019-02-28 15:21:13 +0300

    inputs and server request

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,146 @@
-import React, { Component } from 'react';
-import '../index.css';
-import LEDInput from './LEDInput'
-import pic from '../ill.jpg'
+import React, { Component } from "react";
+import "../index.css";
+import LEDInput from "./LEDInput";
+import { setParameters, setDate, getStatus } from "../services.js";
+import pic from "../ill.jpg";
+
+export const brightnessSliderMax = 10;
+export const speedSliderMax = 10;
 
 class App extends Component {
   state = {
-            speed: '5',
-            brightness: '5',
-            mode: '1',
-            color: '#f8e329',
-            time: '08:00'
-          }
+    currentStatus: "",
+    params: {
+      speed: "5",
+      brightness: "5",
+      mode: "1",
+      color: "#f8e329",
+      time: "08:00"
+    }
+  };
 
-  handleChange = (event) => {
-      this.setState({ [event.target.id]: event.target.value });
-  }
+  componentDidMount = () => {
+    setDate();
+    console.log("DID MOUNT");
+    this.setState({ currentStatus: getStatus() || "noup" });
+  };
 
-  handleSubmit = (event) => {
-     event.preventDefault();
-     console.log(this.state)
-  }
+  handleRefresh = () => {
+    this.setState({ currentStatus: getStatus() || "noup" });
+  };
 
-  getBrightnessInput = () => (<input className='led-input-slider' id='brightness' type='range' min="0" max="10" step="1" value={this.state.brightness} onChange={this.handleChange} />)
-  getSpeedInput = () => (<input className='led-input-slider' id='speed' type='range' min="0" max="10" step="1" value={this.state.speed} onChange={this.handleChange} />)
-  getTimeInput = () => (<input className='led-input-time' id='time' type='time' min="0:00" max="23:59" value={this.state.time} onChange={this.handleChange} />)
-  getColorInput = () => (<input className='led-input-color' id='color' type='color' min="0:00" max="23:59" value={this.state.color} onChange={this.handleChange} />)
-  getModeInput = (modesAmount) => {
-      const renderOption = (mode) => <option value={mode} key={mode}>{mode}</option>
-      const options = []
-      for (let i = 0; i < modesAmount; i++) {
-        options.push(renderOption(i + 1))
-      }
-      return (
-        <select id='mode' className='led-input-mode' onChange={this.handleChange}>
-            {options}
-        </select>
-      )
-  }
+  handleChange = event => {
+    this.setState({
+      params: { ...this.state.params, [event.target.id]: event.target.value }
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    setParameters(this.state.params);
+    console.log("FORM SUBMIT", this.state);
+  };
+
+  getBrightnessInput = () => (
+    <input
+      className="led-input-slider"
+      id="brightness"
+      type="range"
+      min="0"
+      max={brightnessSliderMax}
+      step="1"
+      value={this.state.params.brightness}
+      onChange={this.handleChange}
+    />
+  );
+
+  getSpeedInput = () => (
+    <input
+      className="led-input-slider"
+      id="speed"
+      type="range"
+      min="0"
+      max={speedSliderMax}
+      step="1"
+      value={this.state.params.speed}
+      onChange={this.handleChange}
+    />
+  );
+
+  getTimeInput = () => (
+    <input
+      className="led-input-time"
+      id="time"
+      type="time"
+      min="0:00"
+      max="23:59"
+      value={this.state.params.time}
+      onChange={this.handleChange}
+    />
+  );
+
+  getColorInput = () => (
+    <input
+      className="led-input-color"
+      id="color"
+      type="color"
+      min="0:00"
+      max="23:59"
+      value={this.state.params.color}
+      onChange={this.handleChange}
+    />
+  );
+
+  getModeInput = modesAmount => {
+    const renderOption = mode => (
+      <option value={mode} key={mode}>
+        {mode}
+      </option>
+    );
+    const options = [];
+    for (let i = 0; i < modesAmount; i++) {
+      options.push(renderOption(i + 1));
+    }
+    return (
+      <select id="mode" className="led-input-mode" onChange={this.handleChange}>
+        {options}
+      </select>
+    );
+  };
 
   render() {
-    const brightnessInput = this.getBrightnessInput()
-    const speedInput = this.getSpeedInput()
-    const timeInput = this.getTimeInput()
-    const colorInput = this.getColorInput()
-    const modeInput = this.getModeInput(20)
+    console.log(this.state);
+    const brightnessInput = this.getBrightnessInput();
+    const speedInput = this.getSpeedInput();
+    const timeInput = this.getTimeInput();
+    const colorInput = this.getColorInput();
+    const modeInput = this.getModeInput(20);
     return (
-      <div className='wrapper' key='wrapper'>
-          <form onSubmit={this.handleSubmit}>
-              <LEDInput value={this.state.brightness} input={brightnessInput} label='Brightness' key="brightness" withnum />
-              <LEDInput value={this.state.speed} input={speedInput} label='Speed' key="speed" withnum />
-              <LEDInput value={this.state.time} input={timeInput} label='Time' key="time" />
-              <LEDInput value={this.state.color} input={colorInput} label='Color' key="color" />
-              <LEDInput value={this.state.mode} input={modeInput} label='Mode' key="mode" />
-              <button className='apply-button' htmltype="submit"> Apply </button>
-          </form>
+      <div className="wrapper" key="wrapper">
+        <p>{this.state.currentStatus}</p>
+        <button onClick={this.handleRefresh}>Refresh</button>
+        <form onSubmit={this.handleSubmit}>
+          <LEDInput
+            value={this.state.params.brightness}
+            input={brightnessInput}
+            label="Brightness"
+            key="brightness"
+            withnum
+          />
+          <LEDInput
+            value={this.state.params.speed}
+            input={speedInput}
+            label="Speed"
+            key="speed"
+            withnum
+          />
+          <LEDInput input={timeInput} label="Time" key="time" />
+          <LEDInput input={colorInput} label="Color" key="color" />
+          <LEDInput input={modeInput} label="Mode" key="mode" />
+          <button className="apply-button" htmltype="submit">
+            Apply
+          </button>
+        </form>
       </div>
     );
   }
